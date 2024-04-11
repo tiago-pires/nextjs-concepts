ddf22bba1240d991f30735356dec725bfe626046
Tiago Pires <tiago.pires@me.com>
2024-04-11 15:19:24 +1000

    Add CRUD user routs

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -4,14 +4,24 @@ interface Props {
   params: { id: number };
 }
 
+// Source users form DB, hardcoded here  (localstorage wont work, this happens on the server!)
+function getUserById(id) {
+  const users = [
+    { id: "1", name: "Tiago" },
+    { id: "2", name: "Bruno" },
+    { id: "3", name: "Lucas" },
+  ];
+  const user = users.find((user) => user.id === id);
+
+  if (!user) return false;
+
+  return user;
+}
+
 // Read record `curl http://localhost:3000/api/users/2`
 export async function GET(request, { params: { id } }: Props) {
-  // get users from source (DB etc)
-  const res = await fetch(`http://localhost:3000/api/users/`);
-  const users = await res.json();
-
   // get single user (remember: url id is a string)
-  const user = users.find((user) => user.id === parseInt(id));
+  const user = getUserById(id);
 
   // handle non existing user
   if (!user)
@@ -23,12 +33,39 @@ export async function GET(request, { params: { id } }: Props) {
   return NextResponse.json(user);
 }
 
-// Update
+// Update a user `curl -X PUT  http://localhost:3000/api/users/1 -H "Content-Type: application/json" -d '{"name":"Marcela"}'`
 export async function PUT(request: NextRequest, { params: { id } }: Props) {
-  return NextResponse("put");
+  const body = await request.json();
+
+  if (!body.name) {
+    return NextResponse.json({ error: `Name is required` }, { status: 400 });
+  }
+
+  const user = getUserById(id);
+
+  if (!user) {
+    return NextResponse.json(
+      { error: `User ${id} not found` },
+      { status: 404 },
+    );
+  }
+
+  return NextResponse.json({ id, name: body.name });
 }
 
-// Delete
+// Delete a user `curl -X DELETE  http://localhost:3000/api/users/1`
 export async function DELETE(request: NextRequest, { params: { id } }: Props) {
-  return NextResponse("delete");
+  // Get user (from DB)
+  const user = getUserById(id);
+
+  // If user not found
+
+  if (!user) {
+    return NextResponse.json(
+      { error: `User ${id} not found` },
+      { status: 404 },
+    );
+  }
+  // return deleted user
+  return NextResponse.json(user);
 }
