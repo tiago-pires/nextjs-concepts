28b2b41db3e2535875cb4dbb92eec754f1fd69d2
Tiago Pires <tiago.pires@me.com>
2024-04-13 16:25:13 +1000

    Add users from route using prisma

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -7,16 +7,39 @@ export async function GET(request: NextRequest) {
   return NextResponse.json(users, { status: 200 });
 }
 
-// Create user `curl -X POST http://localhost:3000/api/users -H "Content-Type: application/json" -d '{"name":"Marcela"}'`
+// Create user `curl -X POST http://localhost:3000/api/users -H "Content-Type: application/json" -d '{"name":"Marcela", "email": "m@mo.co"}'`
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
   // validate request body
-  if (!body.name) {
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  if (!body.name || !body.email) {
+    return NextResponse.json(
+      { error: "Name or Email is required" },
+      { status: 400 },
+    );
   }
+
+  // check user with this email does not yet exist
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (existingUser)
+    return NextResponse.json(
+      { error: "User already exisits" },
+      { status: 400 },
+    );
+
   // create user in DB (with new id)
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
 
   // response
-  return NextResponse.json({ todo: "will create user next" }, { status: 201 });
+  return NextResponse.json(newUser, { status: 201 });
 }
