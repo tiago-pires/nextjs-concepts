f86ae6f704661a28f96ed1292516f36ea543128b
Tiago Pires <tiago.pires@me.com>
2024-04-20 11:43:19 +1000

    Fetch only Posts

--- app/fetch/page.tsx
+++ app/fetch/page.tsx
@@ -1,10 +1,3 @@
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
-
 interface Post {
   id: number;
   title: string;
@@ -17,27 +10,14 @@ async function getPosts() {
   return res.json();
 }
 
-async function getUsers() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  if (!res.ok) throw new Error(`Fail to fetch data: Status code ${res.status}`);
-  return res.json();
-}
-
 export default async function Page() {
   const posts: Post[] = await getPosts();
-  const users: User[] = await getUsers();
   return (
     <>
       <h2>Posts</h2>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-      <h1>Users</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
         ))}
       </ul>
     </>
